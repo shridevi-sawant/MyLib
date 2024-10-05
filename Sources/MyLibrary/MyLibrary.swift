2c6e320cc8686cc6cc1663826b8849413b163eeb
shridevi-sawant <shridevi.hiroji@gmail.com>
2024-10-05 19:25:30 +0530

    Initial Commit

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,12 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+public struct LocationUtility {
+    public init() {}
+    
+    public func getLoc() -> Int{
+        
+        return 1
+    }
+}
